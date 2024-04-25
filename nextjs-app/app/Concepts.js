f0370372de68dfaa4922a0c84f647435060b8945
Charith Wijewardena <charithw01@gmail.com>
2024-04-25 16:24:45 +1000

    add all files before updating project

--- nextjs-app/app/Concepts.js
+++ nextjs-app/app/Concepts.js
@@ -1,69 +1,69 @@
-import { Graph } from "@cosmograph/cosmos";
-import nodes from "../data/nodes.json";
-import links from "../data/links.json";
-
-export default function Concepts() {
-  const canvas = document.createElement("canvas");
-
-  const config = {
-    simulation: {
-      repulsion: 0.5,
-    },
-    renderLinks: true,
-    events: {
-      onClick: (node) => {
-        console.log("Clicked node: ", node);
-      },
-    },
-    /* ... */
-  };
-
-  const graph = new Graph(canvas, config);
-
-  graph.setData(nodes, links);
-  console.log(canvas);
-  return canvas;
-}
-
-// import React, { Component } from "react";
-// import * as d3 from "d3";
-
 // import { Graph } from "@cosmograph/cosmos";
 // import nodes from "../data/nodes.json";
 // import links from "../data/links.json";
 
-// class Concepts extends Component {
-//   componentDidMount() {
-//     this.drawChart();
-//   }
-//   drawChart() {
-//     // const canvas = d3
-//     //   .select("body")
-//     //   .append("canvas")
-//     //   .attr("width", 700)
-//     //   .attr("height", 300);
+// export default function Concepts() {
+//   const canvas = document.createElement("canvas");
 
-//     const canvas = document.querySelector("canvas");
-
-//     const config = {
-//       simulation: {
-//         repulsion: 0.5,
+//   const config = {
+//     simulation: {
+//       repulsion: 0.5,
+//     },
+//     renderLinks: true,
+//     events: {
+//       onClick: (node) => {
+//         console.log("Clicked node: ", node);
 //       },
-//       renderLinks: true,
-//       events: {
-//         onClick: (node) => {
-//           console.log("Clicked node: ", node);
-//         },
-//       },
-//       /* ... */
-//     };
+//     },
+//     /* ... */
+//   };
 
-//     const graph = new Graph(canvas, config);
+//   const graph = new Graph(canvas, config);
 
-//     return graph.setData(nodes, links);
-//   }
-//   render() {
-//     return <div id={"#" + this.props.id}></div>;
-//   }
+//   graph.setData(nodes, links);
+//   console.log(canvas);
+//   return canvas;
 // }
-// export default Concepts;
+
+import React, { Component } from "react";
+// import * as d3 from "d3";
+
+import { Graph } from "@cosmograph/cosmos";
+import nodes from "../data/nodes.json";
+import links from "../data/links.json";
+
+class Concepts extends Component {
+  componentDidMount() {
+    this.drawChart();
+  }
+  drawChart() {
+    // const canvas = d3
+    //   .select("body")
+    //   .append("canvas")
+    //   .attr("width", 700)
+    //   .attr("height", 300);
+
+    const canvas = document.querySelector("canvas");
+
+    const config = {
+      simulation: {
+        repulsion: 0.5,
+      },
+      renderLinks: true,
+      events: {
+        onClick: (node) => {
+          console.log("Clicked node: ", node);
+        },
+      },
+      /* ... */
+    };
+
+    const graph = new Graph(canvas, config);
+
+    return graph.setData(nodes, links);
+  }
+  render() {
+    return <div id={"#" + this.props.id}></div>;
+  }
+}
+export default Concepts;
--- nextjs-app/app/page.js
+++ nextjs-app/app/page.js
@@ -4,8 +4,27 @@
 import * as d3 from "d3";
 import BarChart from "./Barchart";
 import Concepts from "./Concepts";
+import pull from "@/utils/pull";
+import Sutta from "@/utils/sutta";
 
 export default function Home() {
+  const characterData = pull(
+    "https://raw.githubusercontent.com/chaz23/buddhism-in-color/main/projects/sutta-characters/data/pli2en_dppn_updated.json"
+  ).json();
+
+  const x = new Sutta("mn1");
+  const y = x.translation;
+
+  if (characterData.isLoading || y.isLoading) return null;
+
+  const sutta = y.data;
+
+  const characters = characterData.data;
+  console.log(characters);
+
+  // if (y.isLoading) return null;
+  console.log(sutta);
+
   return (
     <div>
       <canvas />
