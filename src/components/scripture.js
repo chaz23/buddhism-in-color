c4dbe8f1ac677473b49609ad695d5b8e244e5e14
chaz23 <charithw01@gmail.com>
2025-08-12 19:26:57 +0530

    update scatterplot

--- src/components/scripture.js
+++ src/components/scripture.js
@@ -22,31 +22,31 @@ class Scripture {
     return result.toArray()[0];
   }
 
-  async html() {
-    const query = `SELECT segment_text, html FROM sutta_data WHERE sutta='${this.id}'`;
-    const result = await db.query(query);
-    const html = result
-      .toArray()
-      .map((d) => d.html.replace(/{}/, d.segment_text))
-      .join(" ");
+  // async html() {
+  //   const query = `SELECT segment_text, html FROM sutta_data WHERE sutta='${this.id}'`;
+  //   const result = await db.query(query);
+  //   const html = result
+  //     .toArray()
+  //     .map((d) => d.html.replace(/{}/, d.segment_text))
+  //     .join(" ");
 
-    const div = document.createElement("div");
-    div.innerHTML = `${html}`;
-    return div;
-  }
+  //   const div = document.createElement("div");
+  //   div.innerHTML = `${html}`;
+  //   return div;
+  // }
 
-  async sectionText(section_num) {
-    const query = `
-      SELECT segment_text
-      FROM sutta_data
-      WHERE sutta='${this.id}' 
-        AND section_num='${section_num}'`;
-    const result = await db.query(query);
-    return result
-      .toArray()
-      .map((d) => d.segment_text)
-      .join(" ");
-  }
+  // async sectionText(section_num) {
+  //   const query = `
+  //     SELECT segment_text
+  //     FROM sutta_data
+  //     WHERE sutta='${this.id}'
+  //       AND section_num='${section_num}'`;
+  //   const result = await db.query(query);
+  //   return result
+  //     .toArray()
+  //     .map((d) => d.segment_text)
+  //     .join(" ");
+  // }
 }
 
 export default Scripture;
--- src/index3.md
+++ src/index3.md
@@ -3,41 +3,41 @@
 <!-- Imports. -->
 
 ```js
-import Minimap from "./components/minimap.js";
+// import Minimap from "./components/minimap.js";
 ```
 
 <!-- Data. -->
 
 ```js
 // Do not cache the data on Windows machines due to bug in duckdb-wasm.
-const db = await DuckDBClient.of({
-  sutta_hierarchy:
-    FileAttachment("data/sutta_hierarchy.parquet").href +
-    (navigator.userAgent.includes("Windows") ? `?t=${Date.now()}` : ""),
-});
+// const db = await DuckDBClient.of({
+//   sutta_hierarchy:
+//     FileAttachment("data/sutta_hierarchy.parquet").href +
+//     (navigator.userAgent.includes("Windows") ? `?t=${Date.now()}` : ""),
+// });
 
-const sutta_hierarchy = db.sql`SELECT * FROM sutta_hierarchy`;
+// const sutta_hierarchy = db.sql`SELECT * FROM sutta_hierarchy`;
 ```
 
 <!-- State. -->
 
 ```js
-let state = Mutable({ selectedMinimapNode: null });
-const setState = (key, value) =>
-  (state.value = {
-    ...state.value,
-    [key]: value,
-  });
+// let state = Mutable({ selectedMinimapNode: null });
+// const setState = (key, value) =>
+//   (state.value = {
+//     ...state.value,
+//     [key]: value,
+//   });
 ```
 
 <!-- LAYOUT -->
 
 ```js
-display(state);
+// display(state);
 ```
 
 ```js
 // display(minimap(sutta_hierarchy, setState));
-const x = new Minimap(sutta_hierarchy, setState);
-display(x.visual());
+// const x = new Minimap(sutta_hierarchy, setState);
+// display(x.visual());
 ```
